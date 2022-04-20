38c58d141ebfde387f87a1f17f8055b3d478c6d0
John Doe <hamedhasan422421@gmail.com>
2022-04-20 12:40:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+   
+  }, []);
+const handleSubmit = event => {
+  event.preventDefault();
+  const name = event.target.name.value
+  const email = event.target.email.value
+  const password = event.target.password.value
+ const user = {name, email,password}
+
+ fetch('http://localhost:5000/use', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(user),
+})
+.then(response => response.json())
+.then(data => {
+  const newUser = [...users, data]
+  users.push(newUser)
+  // console.log(newUser)
+  // console.log('Success:', data);
+})
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>total user {users.length}</h2>
+     <form onSubmit={handleSubmit}>
+       <input type="text" name='name' placeholder="text" required/>
+       <input type="email" name="email" placeholder="email" required/>
+       <input type="password" name="password" placeholder="password" required/>
+       <input type="submit" value="send" />
+     </form>
+     {
+       users.map(user => <div key={user.id}>
+         <h2>Name: {user.name}</h2>
+         <h2>Phone: {user.phone}</h2>
+       </div>)
+     }
     </div>
   );
 }
